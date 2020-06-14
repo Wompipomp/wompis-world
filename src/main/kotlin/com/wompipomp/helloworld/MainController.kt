a40b370b5dc3a505965e6dcd2ec0b6d11f94f564
Wompipomp <mark.altmann@satellytes.com>
2020-06-14 20:11:22 +0200

    init

---
+++ src/main/kotlin/com/wompipomp/helloworld/MainController.kt
@@ -0,0 +1,15 @@
+package com.wompipomp.helloworld
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController{
+
+    @GetMapping("/")
+    fun helloWorld() : ResponseEntity<String>
+    {
+        return ResponseEntity.ok("Hello World!!!")
+    }
+}
